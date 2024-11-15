54232a7f1cc59a97184534214fa2c7c180896a83
Anderson Alves De Oliveira Mares <anderson.mares@fazenda.rj.gov.br>
2024-11-15 12:19:08 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/gestaoportifolio/GestaoPortifolioApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestaoportifolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPortifolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoPortifolioApplication.class, args);
+    }
+
+}
